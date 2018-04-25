9e0392b877722759f755422db63bc80a4ca66251
msojka96 <mikolaj.sojka@gmail.com>
2018-04-25 21:53:22 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+document.onreadystatechange = function () {
+
+    Hide = ((text) => {
+        text.forEach((text_list) => {
+            text_list.hidden = true;
+        });
+    });
+
+    if (document.readyState === "interactive") {
+
+        text = document.querySelectorAll(".text");
+        button = document.querySelectorAll(".button");
+
+        Hide(text);
+        document.querySelector("#description").hidden=false;
+    }
+
+Search = ((name) => {
+    text.forEach((text_list) => {
+        if(text_list.id===name){
+            if(text_list){
+                text_list.hidden = false;
+            }
+            else{
+                text_list.hidden = true;
+            }
+        }
+    });
+
+});
+
+    OnClick_Button = ((button,name) => {
+        button.onclick = () => {
+            if(name==="button_description"){
+                Hide(text);
+                Search("description");
+            }
+
+            if(name==="button_programs"){
+                Hide(text);
+                Search("programs");
+            }
+
+            if(name==="button_contact"){
+                Hide(text);
+                Search("contact");
+            }
+        
+        };
+    });
+
+button.forEach((button_list) => {
+    OnClick_Button(button_list,button_list.id);
+});
+};
