3a3dc9cd2d1cef24cc4c7d5b80aeaaad958c26e4
msojka96 <mikolaj.sojka@gmail.com>
2018-04-26 11:00:20 +0200

    active

--- script.js
+++ script.js
@@ -9,49 +9,57 @@ document.onreadystatechange = function () {
     if (document.readyState === "interactive") {
 
         text = document.querySelectorAll(".text");
-        button = document.querySelectorAll(".button");
+        buttons = document.querySelectorAll(".button");
         button_first = document.querySelector("#description");
 
         Hide(text);
-        button_first.hidden=false;
 
+        button_first.hidden = false;
     }
 
-Search = ((name) => {
-    text.forEach((text_list) => {
-        if(text_list.id===name){
-            if(text_list){
-                text_list.hidden = false;
+    Search = ((name) => {
+        text.forEach((text_list) => {
+            if (text_list.id === name) {
+                if (text_list) {
+                    text_list.hidden = false;
+                }
+                else {
+                    text_list.hidden = true;
+                }
             }
-            else{
-                text_list.hidden = true;
-            }
-        }
+        });
     });
 
-});
+    Active = ((button) => {
+        buttons.forEach((button_list) => {
+            button_list.classList.remove("active");
+        });
+        button.classList.add("active");
+    });
 
-    OnClick_Button = ((button,name) => {
+    OnClick_Button = ((button, name) => {
         button.onclick = () => {
-            if(name==="button_description"){
+            Active(button);
+
+            if (name === "button_description") {
                 Hide(text);
                 Search("description");
             }
 
-            if(name==="button_programs"){
+            if (name === "button_programs") {
                 Hide(text);
                 Search("programs");
             }
 
-            if(name==="button_contact"){
+            if (name === "button_contact") {
                 Hide(text);
                 Search("contact");
             }
-        
+
         };
     });
 
-button.forEach((button_list) => {
-    OnClick_Button(button_list,button_list.id);
-});
+    buttons.forEach((button_list) => {
+        OnClick_Button(button_list, button_list.id);
+    });
 };
