fae2ffc2e7680a24e52d3c6c0dcc0135ae595210
msojka96 <mikolaj.sojka@gmail.com>
2019-01-06 15:45:26 +0100

    visibility

--- strona_domki/script1.js
+++ strona_domki/script1.js
@@ -2,13 +2,27 @@ document.onreadystatechange = function () {
 
     if (document.readyState === "interactive") {
 
+        var tempScrollTop = $(window).scrollTop();
+    console.log("Scroll from Top: " + tempScrollTop.toString());
+
         button = document.querySelector("#button_top");
 
         button.onclick = () => {
             document.body.scrollTop = 0; // For Safari
             document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-            console.log("test");
         };
+
+        window.onscroll = function() {
+
+            if(document.documentElement.scrollTop === 0){
+
+                button.style.visibility="hidden";
+                
+            }
+            else{
+                button.style.visibility="visible";
+            }
+        }
 
         
     }
