25444c95f05e2517ee196757e03696528587cf9e
Mikołaj Sojka <msojka@sigma.ug.edu.pl>
2019-09-13 22:50:05 +0200

    k

---
+++ portfolio/precache-manifest.da5db3632c587cadb2234d7a18b88840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71dd5188dbef8a189dd4170b8bb3862",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "75af85d2711c0bd0c382",
+    "url": "/portfolio/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "b9ab01bf771eb4f3602b",
+    "url": "/portfolio/static/css/main.59644df1.chunk.css"
+  },
+  {
+    "revision": "75af85d2711c0bd0c382",
+    "url": "/portfolio/static/js/2.98f9eb44.chunk.js"
+  },
+  {
+    "revision": "b9ab01bf771eb4f3602b",
+    "url": "/portfolio/static/js/main.0f9c371a.chunk.js"
+  },
+  {
+    "revision": "255cf37ac81f61801b31",
+    "url": "/portfolio/static/js/runtime~main.36963274.js"
+  }
+]);
--- portfolio/service-worker.js
+++ portfolio/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.67623f1c9d6496fa9dba30550fa02591.js"
+  "/portfolio/precache-manifest.da5db3632c587cadb2234d7a18b88840.js"
 );
 
 self.addEventListener('message', (event) => {
