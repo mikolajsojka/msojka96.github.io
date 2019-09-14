b1280ff8005dd682892aa2a0ed25a610c2ac395e
Mikołaj Sojka <msojka@sigma.ug.edu.pl>
2019-09-14 15:16:56 +0200

    m

---
+++ portfolio/precache-manifest.ddc91b6db54b155c9c744d7157d9f1e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9bf9d812ffc6b0c756340b0c40eed9c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "75af85d2711c0bd0c382",
+    "url": "/portfolio/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "ea980c90fd7d7e7ad6bb",
+    "url": "/portfolio/static/css/main.7cacb925.chunk.css"
+  },
+  {
+    "revision": "75af85d2711c0bd0c382",
+    "url": "/portfolio/static/js/2.98f9eb44.chunk.js"
+  },
+  {
+    "revision": "ea980c90fd7d7e7ad6bb",
+    "url": "/portfolio/static/js/main.6ef206ea.chunk.js"
+  },
+  {
+    "revision": "255cf37ac81f61801b31",
+    "url": "/portfolio/static/js/runtime~main.36963274.js"
+  }
+]);
