f7463d3f873a6ecd96cec87fadd50f0b6dc63929
Mikołaj Sojka <msojka@sigma.ug.edu.pl>
2019-10-17 12:59:44 +0200

    kkk

---
+++ searcher/precache-manifest.8bb020ea795ad9a9fe759ac202736be3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81a37556090cecc84a7e768495febcf4",
+    "url": "/searcher/index.html"
+  },
+  {
+    "revision": "5c9e272da590d2463d60",
+    "url": "/searcher/static/css/main.404ba3bb.chunk.css"
+  },
+  {
+    "revision": "46724945b04f95d989c4",
+    "url": "/searcher/static/js/2.4f5fb654.chunk.js"
+  },
+  {
+    "revision": "5c9e272da590d2463d60",
+    "url": "/searcher/static/js/main.865beec6.chunk.js"
+  },
+  {
+    "revision": "ce0f2f7e1e6c68c15c48",
+    "url": "/searcher/static/js/runtime-main.70a48d51.js"
+  }
+]);
--- searcher/service-worker.js
+++ searcher/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cd9a982da5849d96524c62b63c104647.js"
+  "/searcher/precache-manifest.8bb020ea795ad9a9fe759ac202736be3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/searcher/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
