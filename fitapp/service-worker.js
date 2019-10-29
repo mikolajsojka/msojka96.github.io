a1c812af3464789c659a1aa3bd08278d2a552308
Mikołaj Sojka <msojka@sigma.ug.edu.pl>
2019-10-29 21:38:23 +0100

    kkkk

--- fitapp/service-worker.js
+++ fitapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.74d7106d9becf8d1cdb0f83732059e4b.js"
+  "/fitapp/precache-manifest.464029ed29b60be0105142d126c294a5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fitapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
