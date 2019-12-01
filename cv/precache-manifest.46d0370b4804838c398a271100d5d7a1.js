8c4e3c0dfa4e170d7a9a79326bbd85c9b04a2228
Mikołaj Sojka <msojka@sigma.ug.edu.pl>
2019-12-01 16:20:51 +0100

    pp

---
+++ cv/precache-manifest.46d0370b4804838c398a271100d5d7a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11d9c71944f5779bfce5504225c098d",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "6cc9a3e24fabfe7ed12f",
+    "url": "/cv/static/css/main.edd248e7.chunk.css"
+  },
+  {
+    "revision": "f07826dcdec4482b32bb",
+    "url": "/cv/static/js/2.edf21df3.chunk.js"
+  },
+  {
+    "revision": "6cc9a3e24fabfe7ed12f",
+    "url": "/cv/static/js/main.080a60ff.chunk.js"
+  },
+  {
+    "revision": "f6a2ed6f2e54ddaa6ed8",
+    "url": "/cv/static/js/runtime-main.a182132d.js"
+  },
+  {
+    "revision": "1f9af3fed42a7a46a926eca7e5c6eec0",
+    "url": "/cv/static/media/a.1f9af3fe.png"
+  },
+  {
+    "revision": "dd2e54e97b566b72606b5556a18f1dbd",
+    "url": "/cv/static/media/b.dd2e54e9.png"
+  },
+  {
+    "revision": "9ed6420347459b7c0094f359c4f8b2c1",
+    "url": "/cv/static/media/c.9ed64203.png"
+  }
+]);
