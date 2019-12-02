e796af0d751c336e901ca8dc7203923889860c8e
Mikołaj Sojka <msojka@sigma.ug.edu.pl>
2019-12-02 12:38:38 +0100

    kk

---
+++ calories/precache-manifest.1b1bf3123718d683a7f2fec5e3ac9908.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "002751ed621d06db1cb7ded1f51155db",
+    "url": "/calories/index.html"
+  },
+  {
+    "revision": "dd9943dc1e22fc682e44",
+    "url": "/calories/static/css/main.af2c56be.chunk.css"
+  },
+  {
+    "revision": "d8c47a635881d3807777",
+    "url": "/calories/static/js/2.03b9d4f7.chunk.js"
+  },
+  {
+    "revision": "dd9943dc1e22fc682e44",
+    "url": "/calories/static/js/main.a5d6a63e.chunk.js"
+  },
+  {
+    "revision": "429d73e47c36b2a7e906",
+    "url": "/calories/static/js/runtime-main.27f6043a.js"
+  }
+]);
