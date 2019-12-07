5332bdb50e284d4e135401889fe9dfc666607773
Mikołaj Sojka <msojka@sigma.ug.edu.pl>
2019-12-07 16:47:41 +0100

    kk

---
+++ calculators/precache-manifest.bf854502c26ddaf8d66093fd9c6a06ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "298524291eef4c124c535b06437d27e8",
+    "url": "/calculators/index.html"
+  },
+  {
+    "revision": "eba65f4b517c3c8102e9",
+    "url": "/calculators/static/css/main.0e3cf46d.chunk.css"
+  },
+  {
+    "revision": "6ccee90ce7b483892194",
+    "url": "/calculators/static/js/2.ba078b46.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calculators/static/js/2.ba078b46.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eba65f4b517c3c8102e9",
+    "url": "/calculators/static/js/main.5151b0c6.chunk.js"
+  },
+  {
+    "revision": "8f91cedbc3b5c9350259",
+    "url": "/calculators/static/js/runtime-main.14707771.js"
+  }
+]);
