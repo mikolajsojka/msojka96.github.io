83762fa015233c839c061e327e8fa161a62ca3a6
Mikołaj Sojka <msojka@sigma.ug.edu.pl>
2019-12-19 00:12:20 +0100

    s

---
+++ domki/precache-manifest.720e64d2224619d37dc152aa7cdde7c1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d17244253d72d587a7cfa6849ce1e34",
+    "url": "/domki/index.html"
+  },
+  {
+    "revision": "33fa8208564607114f00",
+    "url": "/domki/static/css/2.62027c08.chunk.css"
+  },
+  {
+    "revision": "6098e04147a0d3b2a9fc",
+    "url": "/domki/static/css/main.368a136e.chunk.css"
+  },
+  {
+    "revision": "33fa8208564607114f00",
+    "url": "/domki/static/js/2.dc769a6d.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/domki/static/js/2.dc769a6d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6098e04147a0d3b2a9fc",
+    "url": "/domki/static/js/main.01680af5.chunk.js"
+  },
+  {
+    "revision": "9ae95b6e82ec49b6488d",
+    "url": "/domki/static/js/runtime-main.58859bc7.js"
+  },
+  {
+    "revision": "46b42f091ef64f85139e223132275441",
+    "url": "/domki/static/media/1.46b42f09.jpg"
+  },
+  {
+    "revision": "855efa2d9d16a7b9551ec1d253ead7b5",
+    "url": "/domki/static/media/1.855efa2d.jpg"
+  },
+  {
+    "revision": "6bdcb53003fca978f191a2fbe16f1f3e",
+    "url": "/domki/static/media/2.6bdcb530.jpg"
+  },
+  {
+    "revision": "96c0d9fad562bdd5ebcdaa53e3a58eac",
+    "url": "/domki/static/media/3.96c0d9fa.jpg"
+  }
+]);
