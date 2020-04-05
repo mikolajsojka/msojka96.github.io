8fda3c6f8458b365f982b13ea3be4ea7fdce7a70
Mikolaj <mikolaj.sojka@gmail.com>
2020-04-05 14:02:20 +0200

    changed skm

--- skm/service-worker.js
+++ skm/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/skm/precache-manifest.9a0b99f4e3c49db582abe555f6a8b150.js"
+  "/skm/precache-manifest.ec1e89bd179a962f1edba5277bb2ff57.js"
 );
 
 self.addEventListener('message', (event) => {
