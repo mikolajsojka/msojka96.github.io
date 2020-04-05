1c8dc4a08d05ac1fa1c9556a1521a32c69991619
Mikolaj <mikolaj.sojka@gmail.com>
2020-04-05 14:07:10 +0200

    update

---
+++ skm/precache-manifest.ec1e89bd179a962f1edba5277bb2ff57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76bc52742c90287b2210ad2bafb2694",
+    "url": "/skm/index.html"
+  },
+  {
+    "revision": "0acc7053b6a54e703c73",
+    "url": "/skm/static/css/main.e2bcb110.chunk.css"
+  },
+  {
+    "revision": "4b4e8f24090205c9fd8f",
+    "url": "/skm/static/js/2.0557801e.chunk.js"
+  },
+  {
+    "revision": "0acc7053b6a54e703c73",
+    "url": "/skm/static/js/main.bf1250c2.chunk.js"
+  },
+  {
+    "revision": "48413e07b647ad58d535",
+    "url": "/skm/static/js/runtime-main.49400f36.js"
+  },
+  {
+    "revision": "d31a5c9a93492aa8baf133ab5ec41125",
+    "url": "/skm/static/media/bckg.d31a5c9a.png"
+  }
+]);
